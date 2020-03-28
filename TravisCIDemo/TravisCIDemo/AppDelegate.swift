c03725c6dacaa2307d41771998c223412a1859ab
yujinhai <yujinhai2019@163.com>
2020-03-29 00:26:55 +0800

    add new project

---
+++ TravisCIDemo/TravisCIDemo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TravisCIDemo
+//
+//  Created by yujinhai on 2020/3/29.
+//  Copyright © 2020 yujinhai. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ TravisCIDemo/TravisCIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravisCIDemo
+//
+//  Created by yujinhai on 2020/3/29.
+//  Copyright © 2020 yujinhai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
