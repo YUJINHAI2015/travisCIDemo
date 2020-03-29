978bfb504016e81889cc7762a4b77b682d29fc56
yujinhai <yujinhai2019@163.com>
2020-03-30 01:12:12 +0800

    12e23e231

--- build/Pods.build/Release-iphoneos/Pods-TravisCIDemo.build/DerivedSources/Pods_TravisCIDemo_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char Pods_TravisCIDemoVersionString[];
- extern const double Pods_TravisCIDemoVersionNumber;
-
- const unsigned char Pods_TravisCIDemoVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_TravisCIDemo  PROJECT:Pods-1" "\n";
- const double Pods_TravisCIDemoVersionNumber __attribute__ ((used)) = (double)1.;
